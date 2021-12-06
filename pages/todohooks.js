4b7940cedf4e976f7a823a33590da3f69d4ccdb8
dylan <dylanwozny14@gmail.com>
2021-12-05 23:15:08 -0700

    commented out todo to fix react use call

--- pages/todohooks.js
+++ pages/todohooks.js
@@ -1,46 +1,46 @@
-// external data import
-import { useState, useContext,useEffect,useReducer,useRef} from "react";
-import useSWR from "swr";
-import firebase from "../firebase";
-import Brand from "../components/branding"
-import NavBar from "../components/navbar";
-import {Button} from 'components/ui/buttons';
-import styled from "styled-components";
-import { List, ListItem } from "components/ui/list";
-import { useRouter } from "next/router";
+// // external data import
+// import { useState, useContext,useEffect,useReducer,useRef} from "react";
+// import useSWR from "swr";
+// import firebase from "../firebase";
+// import Brand from "../components/branding"
+// import NavBar from "../components/navbar";
+// import {Button} from 'components/ui/buttons';
+// import styled from "styled-components";
+// import { List, ListItem } from "components/ui/list";
+// import { useRouter } from "next/router";
 
 
-// HOOK is a reusable function
+// // HOOK is a reusable function
 
 
-const Wrapper = styled.div``;
+// const Wrapper = styled.div``;
 
 
-const fetcher = (...args) => fetch(...args).then(res => res.json())
+// const fetcher = (...args) => fetch(...args).then(res => res.json())
 
-// 
- function toDo(props) {
-  //  react function
-   const [isValidUser, setValidUser] = useState(false);
-  //  setting router
-  const router = useRouter();
+// // 
+//  function toDo(props) {
+//   //  react function
+//    const [isValidUser, setValidUser] = useState(false);
+//   //  setting router
+//   const router = useRouter();
 
-   function onRequestSignIn (){
-     setValidUser(true);
-   }
+//    function onRequestSignIn (){
+//      setValidUser(true);
+//    }
 
-   if(isValidUser){
-     router.push('http://www.apple.com');
+//    if(isValidUser){
+//      router.push('http://www.apple.com');
 
-   }
+//    }
 
 
-  return (
-      <Wrapper>
+//   return (
+//       <Wrapper>
 
-          <Button onClick={onRequestSignIn}>Counting</Button>
-      </Wrapper>
-    )
-}
+//           <Button onClick={onRequestSignIn}>Counting</Button>
+//       </Wrapper>
+//     )
+// }
 
-export default toDo;
+// export default toDo;
