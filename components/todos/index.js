acff052e1e4d0b4a2a43a1bc94a909f03cd80c18
dylan <dylanwozny14@gmail.com>
2022-04-24 13:47:46 -0600

    fixed captialize on component

--- components/todos/index.js
+++ components/todos/index.js
@@ -1,6 +1,6 @@
-export {default as ContentSectionHeader} from './add-edit/ContentSectionHeader';
-export {default as UniqueId} from './add-edit/UniqueId'
-export {default as ContentSection} from './add-edit/ContentSection'
-export {default as AddNewItemForm} from './add-edit/AddNewItemForm'
-export {default as ItemTextArea} from './add-edit/ItemTextArea'
-export {default as ItemIsDone} from './add-edit/itemIsDone'
+export { default as ContentSectionHeader } from "./add-edit/ContentSectionHeader";
+export { default as UniqueId } from "./add-edit/UniqueId";
+export { default as ContentSection } from "./add-edit/ContentSection";
+export { default as AddNewItemForm } from "./add-edit/AddNewItemForm";
+export { default as ItemTextArea } from "./add-edit/ItemTextArea";
+export { default as ItemIsDone } from "./add-edit/ItemIsDone";
