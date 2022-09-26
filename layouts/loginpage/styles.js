54540b8e63ca6367554a939c17a0c09c6c39aeec
Dylan Wozny <dylanwozny14@gmail.com>
2022-09-26 15:54:44 -0600

    Update styles.js

--- layouts/loginpage/styles.js
+++ layouts/loginpage/styles.js
@@ -2,8 +2,6 @@ import styled from "styled-components";
 
 const PageLayout = styled.div`
   max-width: 460px;
-  margin: 3rem auto 0;
-  padding: 3rem 2rem;
   background-color: white;
   border: 3px;
   box-shadow: 0 0 2px 1px rgba(0, 0, 0, 0.125);
